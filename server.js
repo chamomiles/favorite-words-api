248a7edf7235582da22b7afeeb3111183d129452
Chamomile <tima.chamomile@gmail.com>
2023-05-29 12:41:50 +0300

    Set server up

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+PORT = 1000
+
+app.use(express.json())
+
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`)
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="font-family: Arial">My favorite words!</h1>')
+})
