2e8756321fb3858a01b6feec974d8a6e501e5b0d
Chamomile <tima.chamomile@gmail.com>
2023-05-29 12:47:54 +0300

    Add words array

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 PORT = 1000
 
+words = [
+    {
+        id: 1,
+        word: 'Cinnamon',
+        definition: 'An aromatic spice made from the peeled, dried, and rolled bark of a Southeast Asian tree.'
+    },
+    {
+        id: 2,
+        word: 'Sapphire',
+        definition: 'A transparent precious stone, typically blue, that is a variety of corundum (aluminum oxide).'
+    },
+    {
+        id: 3,
+        word: 'Vespertine',
+        definition: 'Relating to, occurring, or active in the evening.'
+    }
+]
+
 app.use(express.json())
 
 app.listen(PORT)
