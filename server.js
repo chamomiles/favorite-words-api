9da927ba6e61a00829357804a622b0030b13706d
Chamomile <tima.chamomile@gmail.com>
2023-05-29 12:44:19 +0300

    Add / HTML documentation

--- server.js
+++ server.js
@@ -8,5 +8,8 @@ app.listen(PORT)
 console.log(`Server is running on port ${PORT}`)
 
 app.get('/', (req, res) => {
-    res.send('<h1 style="font-family: Arial">My favorite words!</h1>')
+    res.send(`
+    <h1 style="font-family: Arial">My favorite words!</h1>
+    <p style="font-family: Arial">This server is for API use only. Please use the '<b>/api/words</b>' route.</p>
+    `)
 })
